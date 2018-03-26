74e6f9a70202e543f2aff33114a0ee6bc3ce3ae7
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-03-25 14:50:12 -1000

    form

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -2,7 +2,19 @@ import React,{Component} from 'react'
 
 class AddPhoto extends Component {
   render() {
-    return <h1>This is the Page with the photos</h1>
+    return (
+      <div>
+      <h1>This is the Page with the photos</h1>
+      <div className="form">
+      <form>
+        <input type="text" placeholder="Link"/>
+        <input type="text" placeholder="Description"/>
+        <button>Post</button>
+      </form>
+    </div>
+    </div>
+
+    )
   }
 }
 
