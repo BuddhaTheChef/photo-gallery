a292aedd53333373afbbf4485dc209952a63868a
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-03-29 14:23:40 -1000

    adding components

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,6 +4,7 @@ import PhotoWall from './PhotoWall'
 import AddPhoto from './AddPhoto'
 import {Route, Link} from 'react-router-dom'
 import {removePost} from '../redux/actions'
+import Single from './Single'
 
 
 class Main extends Component {
@@ -28,6 +29,10 @@ class Main extends Component {
 
       <Route path= "/AddPhoto" render={({history}) => (
         <AddPhoto {...this.props} onHistory={history}/>
+      )}/>
+
+      <Route path="/single/:id" render = {(params) => (
+        <Single {...this.props} {...params}/>
       )}/>
 
       </div>
